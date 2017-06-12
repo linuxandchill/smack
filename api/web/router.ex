4df6047cd211c2bd342e662f010ed3c143e23d8d
rivercodi <rivercodi@gmail.com>
2017-06-12 12:47:09 -0700

    backedn user auth

--- api/web/router.ex
+++ api/web/router.ex
@@ -1,11 +1,34 @@
 defmodule Smack.Router do
   use Smack.Web, :router
 
+use Smack.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/", Smack do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api", Smack do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
   end
 end
--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -1,18 +1,11 @@
 defmodule Smack.UserView do
   use Smack.Web, :view
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, Smack.UserView, "user.json")}
-  end
-
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, Smack.UserView, "user.json")}
-  end
-
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       username: user.username,
       email: user.email,
-      password_hash: user.password_hash}
+    }
   end
 end
